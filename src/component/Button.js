6475e71f57f41ce1528718ff6577b35214545145
Shota <shotikoavaliani97@gmail.com>
2022-04-24 23:11:09 +0400

    main logic is done

--- src/component/Button.js
+++ src/component/Button.js
@@ -3,9 +3,9 @@ import './Slider.css'
 import Next from './Icon/Next.png'
 import Prev from './Icon/Previous.png'
 
-function Button() {
+function Button({moveSlide, direction}) {
   return (
-    <button> <img src={Next} alt='next' /> </button>
+    <button className={direction === "next" ? "btn-slide next" : "btn-slide prev"} onClick={moveSlide}> <img src={direction === 'next' ? Next : Prev} alt='next' /> </button>
   )
 }
 
--- src/component/Slider.js
+++ src/component/Slider.js
@@ -1,18 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 import DataSlider from './DataSlider'
 import Button from './Button'
 import './Slider.css'
 
 function Slider() {
+    const [slide, setSlide]=useState(1)
+    const nextSlide =()=>{
+        if(slide !== DataSlider.length){
+            setSlide(slide + 1)
+        }
+        else if (slide === DataSlider.length){
+            setSlide(1)
+        }
+    }
+    const prevSlide =()=>{
+
+    }
   return (
     <div className='container-slider'>
         {DataSlider.map((obj, index)=>{
             return (
-                <div className='slider'> <img src={process.env.PUBLIC_URL + `/assets/img${index + 1}.jpg`} /> </div>
+                <div className={slide === index + 1 ? 'slide active-anim' : 'slider'}> <img src={process.env.PUBLIC_URL + `/assets/img${index + 1}.jpg`} /> </div>
             )
         })}
-        <Button />
-        <Button />
+        <Button moveSlide={nextSlide} direction='next' />
+        <Button moveSlide={prevSlide} direction='prev'/>
     </div>
   )
 }
